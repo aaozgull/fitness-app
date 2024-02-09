59199c087a9023c41a7c8a8ef4245390e99d3d19
Aaoz Gull <aaozgull@gmail.com>
2024-02-09 23:05:34 +0500

    Delete files to change design

--- src/services/authentication/authentication.service.js
+++
@@ -1,8 +0,0 @@
-/* import * as firebase from "firebase";
-const loginRequest = (email, password) =>
-  firebase.auth().signInWithEmailAndPassword(email, password); */
-/* eslint-disable prettier/prettier */
-import { signInWithEmailAndPassword } from "firebase/auth";
-
-export const loginRequest = (auth, email, password) =>
-  signInWithEmailAndPassword(auth, email, password);
